eac0c2921250639c45e98a133668078f0c2de503
antoshayastreb <nedanon@outlook.com>
2023-10-31 22:45:50 +0300

    log_level

--- jmekbot/__main__.py
+++ jmekbot/__main__.py
@@ -1,10 +1,12 @@
+import os
+
 from aiogram import Dispatcher
 from aiogram.utils import executor
 from jmekbot.loader import dp
 from loguru import logger
 
 from jmekbot.commands import set_default_commands
-from jmekbot.background import keep_alive
+#from jmekbot.background import keep_alive
 
 async def startup(dp: Dispatcher) -> None:
     await set_default_commands(dp)
@@ -13,10 +15,22 @@ async def startup(dp: Dispatcher) -> None:
 async def shutdown(dp: Dispatcher) -> None:
     logger.info('bot finished')
 
+def _get_log_level(env_value: any):
+    if not isinstance(env_value, str):
+        return 'INFO'
+    env_value = env_value.lower()
+    if env_value == 'debug':
+        return 'DEBUG'
+    elif env_value == 'warning':
+        return 'WARNING'
+    else:
+        return 'INFO'
+
 if __name__ == '__main__':
+    log_level = _get_log_level(os.getenv('LOG_LEVEL'))
     logger.add(
         'logs/debug.log',
-        level='DEBUG',
+        level=log_level,
         format='{time} | {level} | {module}:{function}:{line} | {message}',
         rotation='30 KB',
         compression='zip',
